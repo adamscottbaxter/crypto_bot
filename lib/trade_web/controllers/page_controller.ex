db9831627c387e5f43e1ff48bd6883d1fb4c100e
Adam <adam@zacs-mac.attlocal.net>
2019-03-10 15:30:30 -0500

    Add account balances to index view.

--- lib/trade_web/controllers/page_controller.ex
+++ lib/trade_web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule TradeWeb.PageController do
   use TradeWeb, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    balances = Trading.Account.balances()
+    render(conn, "index.html", balances: balances)
   end
 end
